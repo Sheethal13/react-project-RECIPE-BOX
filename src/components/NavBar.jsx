028b9d1e5183567b7ab8dd5ab645ea9cc1d3fae8
Sheethal Sukumar <sheethal.p13@gmail.com>
2024-10-26 09:49:31 +0530

    navbar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function NavBar(){
     const [isFixed,setIsFixed]=useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+      };
     useEffect(()=>{
         
         function handleScroll(){
@@ -24,17 +28,32 @@ export default function NavBar(){
 
     return(
         <nav className={ isFixed?  "fixed-nav" : "nav"}>
-        <ul className="nav-list"> 
-        <li id="something-left">
+        <div className="navbar-left">
+        <p className="navbar-logo">
             <Link to="/challenges">
             Logo
             </Link>
-        </li>
+        </p>
+        {isFixed &&(
+            <h1 className="navbar-heading">
+                RECIPE BOX
+            </h1>
+        )}
+        </div>
+        {/* Hamburger icon for mobile */}
+        <div className="navbar-hamburger" onClick={toggleMenu}>
+            <span className="hamburger-icon"></span>
+            <span className="hamburger-icon"></span>
+            <span className="hamburger-icon"></span>
+        </div>
+        {/* Right side: Menu items (conditionally shown on mobile) */}
+        <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
         <li>Logo</li>
         <li>Logo</li>
         <li>Logo</li>
-        <li>Logo</li>    
+        <li>Logo</li> 
         </ul>
         </nav>
+        
     )
 }
